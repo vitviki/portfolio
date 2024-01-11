4c7f6520683bbd879d454442357ef8f1120ea8d4
vitviki <vtyagi1991@gmail.com>
2024-01-11 13:16:57 +0530

    responsive nagivation

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu_links");
+  const icon = document.querySelector(".hamburger_icon");
+
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
